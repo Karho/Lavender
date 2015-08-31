ae693d2fad9f07293b15a6577c4223b9bf5be033
Karho <Karho.Yau@gmail.com>
2015-08-31 11:58:24 +0800

    A little bit of changes

--- aikon square loader.user.js
+++ aikon square loader.user.js
@@ -1,9 +1,12 @@
 // ==UserScript==
-// @name  aikon square loader
-// @icon  http://na.cx/i/1wng8.gif
-// @include /^https?://.*hkgalden\.com\/(view|reply|post|poll).*$/
-// @require https://pastebin.com/raw.php?i=FLKAxN7u
-// @grant GM_xmlhttpRequest
+// @name  			aikon square loader
+// @author  		松岡禎丞 (id: 3627)
+// @version 		0.0.1
+// @description  	adding more emoticons to HKGalden
+// @icon  			http://na.cx/i/1wng8.gif
+// @include 		/^https?://.*hkgalden\.com\/(view|reply|post|poll).*$/
+// @require 		https://pastebin.com/raw.php?i=FLKAxN7u
+// @grant 			GM_xmlhttpRequest
 // ==/UserScript==
 $('#gb .actp a[href="/member/logout"]').length?GM_xmlhttpRequest({method:'get',
-url:'https://na.cx/ibice',onload:function(r){CoffeeScript.run(r.responseText)}}):0;
+url:'https://na.cx/ibice',onload:function(r){CoffeeScript.run(r.responseText)}}):0;33
